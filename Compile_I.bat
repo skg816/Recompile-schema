e0128a3975c5074c80a6dc080604b80ad591b213
Konstantin Starodubtsev <skg816@gmail.com>
2026-03-15 06:50:46 +0500

    Update database connection credentials in batch file

--- Compile_I.bat
+++ Compile_I.bat
@@ -1,15 +1,15 @@
-plus80w ibs/lena@tuz @Compile_Opers.sql
-plus80w ibs/lena@tuz @recompile_first.sql
-plus80w ibs/lena@tuz @recompile_second.sql
+plus80w username/password@tuz @Compile_Opers.sql
+plus80w username/password@tuz @recompile_first.sql
+plus80w username/password@tuz @recompile_second.sql
 
-plus80w ibs/lena@tuz @Compile_Opers.sql
-plus80w ibs/lena@tuz @recompile_first.sql
-plus80w ibs/lena@tuz @recompile_second.sql
+plus80w username/password@tuz @Compile_Opers.sql
+plus80w username/password@tuz @recompile_first.sql
+plus80w username/password@tuz @recompile_second.sql
 
-plus80w ibs/lena@tuz @Compile_Opers.sql
-plus80w ibs/lena@tuz @recompile_first.sql
-plus80w ibs/lena@tuz @recompile_second.sql
+plus80w username/password@tuz @Compile_Opers.sql
+plus80w username/password@tuz @recompile_first.sql
+plus80w username/password@tuz @recompile_second.sql
 
-plus80w ibs/lena@tuz @Compile_Opers.sql
-plus80w ibs/lena@tuz @recompile_first.sql
-plus80w ibs/lena@tuz @recompile_second.sql
+plus80w username/password@tuz @Compile_Opers.sql
+plus80w username/password@tuz @recompile_first.sql
+plus80w username/password@tuz @recompile_second.sql
