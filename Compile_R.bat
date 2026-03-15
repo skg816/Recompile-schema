dc2c96523a7e61ada90438165b1f0a9e0609eb3f
Konstantin Starodubtsev <skg816@gmail.com>
2026-03-15 06:46:35 +0500

    Add SQL compilation commands to Compile_R.bat
    Added commands to compile and recompile SQL scripts.

---
+++ Compile_R.bat
@@ -0,0 +1,15 @@
+plus80w ibs/talisman@retail @Compile_Opers.sql
+plus80w ibs/talisman@retail @recompile_first.sql
+plus80w ibs/talisman@retail @recompile_second.sql
+
+plus80w ibs/talisman@retail @Compile_Opers.sql
+plus80w ibs/talisman@retail @recompile_first.sql
+plus80w ibs/talisman@retail @recompile_second.sql
+
+plus80w ibs/talisman@retail @Compile_Opers.sql
+plus80w ibs/talisman@retail @recompile_first.sql
+plus80w ibs/talisman@retail @recompile_second.sql
+
+plus80w ibs/talisman@retail @Compile_Opers.sql
+plus80w ibs/talisman@retail @recompile_first.sql
+plus80w ibs/talisman@retail @recompile_second.sql
