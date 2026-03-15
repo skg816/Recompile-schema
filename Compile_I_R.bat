06fada2b7dc359f7d7f9a6acc200ec712cc8de67
Konstantin Starodubtsev <skg816@gmail.com>
2026-03-15 06:45:10 +0500

    Add batch commands for compiling and recompiling

---
+++ Compile_I_R.bat
@@ -0,0 +1,36 @@
+plus80w ibs/lena@tuz @Compile_Opers.sql
+plus80w ibs/lena@tuz @recompile_first.sql
+plus80w ibs/lena@tuz @recompile_second.sql
+
+plus80w ibs/lena@tuz @Compile_Opers.sql
+plus80w ibs/lena@tuz @recompile_first.sql
+plus80w ibs/lena@tuz @recompile_second.sql
+
+plus80w ibs/lena@tuz @Compile_Opers.sql
+plus80w ibs/lena@tuz @recompile_first.sql
+plus80w ibs/lena@tuz @recompile_second.sql
+
+plus80w ibs/lena@tuz @Compile_Opers.sql
+plus80w ibs/lena@tuz @recompile_first.sql
+plus80w ibs/lena@tuz @recompile_second.sql
+
+
+
+
+
+
+plus80w ibs/talisman@retail @Compile_Opers.sql
+plus80w ibs/talisman@retail @recompile_first.sql
+plus80w ibs/talisman@retail @recompile_second.sql
+
+plus80w ibs/talisman@retail @Compile_Opers.sql
+plus80w ibs/talisman@retail @recompile_first.sql
+plus80w ibs/talisman@retail @recompile_second.sql
+
+plus80w ibs/talisman@retail @Compile_Opers.sql
+plus80w ibs/talisman@retail @recompile_first.sql
+plus80w ibs/talisman@retail @recompile_second.sql
+
+plus80w ibs/talisman@retail @Compile_Opers.sql
+plus80w ibs/talisman@retail @recompile_first.sql
+plus80w ibs/talisman@retail @recompile_second.sql
