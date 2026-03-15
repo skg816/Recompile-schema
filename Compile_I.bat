6815caf3a54bb5bffbdb5077a3bb46358b482209
Konstantin Starodubtsev <skg816@gmail.com>
2026-03-15 06:44:10 +0500

    Add Compile_I.bat for SQL compilation scripts

---
+++ Compile_I.bat
@@ -0,0 +1,15 @@
+plus80w ibs/lena@tuz @Compile_Opers.sql
+plus80w ibs/lena@tuz @recompile_first.sql
+plus80w ibs/lena@tuz @recompile_second.sql
+
+plus80w ibs/lena@tuz @Compile_Opers.sql
+plus80w ibs/lena@tuz @recompile_first.sql
+plus80w ibs/lena@tuz @recompile_second.sql
+
+plus80w ibs/lena@tuz @Compile_Opers.sql
+plus80w ibs/lena@tuz @recompile_first.sql
+plus80w ibs/lena@tuz @recompile_second.sql
+
+plus80w ibs/lena@tuz @Compile_Opers.sql
+plus80w ibs/lena@tuz @recompile_first.sql
+plus80w ibs/lena@tuz @recompile_second.sql
