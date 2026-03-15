d251c4cdcacb2deb5044ffe1768895bd757f16ec
Konstantin Starodubtsev <skg816@gmail.com>
2026-03-15 06:53:26 +0500

    Update database connection credentials in batch file

--- Compile_R.bat
+++ Compile_R.bat
@@ -1,15 +1,15 @@
-plus80w ibs/talisman@retail @Compile_Opers.sql
-plus80w ibs/talisman@retail @recompile_first.sql
-plus80w ibs/talisman@retail @recompile_second.sql
+plus80w username/password@retail @Compile_Opers.sql
+plus80w username/password@retail @recompile_first.sql
+plus80w username/password@retail @recompile_second.sql
 
-plus80w ibs/talisman@retail @Compile_Opers.sql
-plus80w ibs/talisman@retail @recompile_first.sql
-plus80w ibs/talisman@retail @recompile_second.sql
+plus80w username/password@retail @Compile_Opers.sql
+plus80w username/password@retail @recompile_first.sql
+plus80w username/password@retail @recompile_second.sql
 
-plus80w ibs/talisman@retail @Compile_Opers.sql
-plus80w ibs/talisman@retail @recompile_first.sql
-plus80w ibs/talisman@retail @recompile_second.sql
+plus80w username/password@retail @Compile_Opers.sql
+plus80w username/password@retail @recompile_first.sql
+plus80w username/password@retail @recompile_second.sql
 
-plus80w ibs/talisman@retail @Compile_Opers.sql
-plus80w ibs/talisman@retail @recompile_first.sql
-plus80w ibs/talisman@retail @recompile_second.sql
+plus80w username/password@retail @Compile_Opers.sql
+plus80w username/password@retail @recompile_first.sql
+plus80w username/password@retail @recompile_second.sql
